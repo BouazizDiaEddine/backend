60eecdd17f9da19d2a1fa9c9ddfac417ac412bd8
dbu <dbu@contraste.com>
2024-05-14 22:56:58 +0100

    db migration

---
+++ migrate/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"backend/initialazers"
+	"backend/models"
+)
+
+func init() {
+	initialazers.LoadEnvVariables()
+	initialazers.ConnectToDb()
+}
+
+func main() {
+	initialazers.DB.AutoMigrate(&models.Book{})
+}
