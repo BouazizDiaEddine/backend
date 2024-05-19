6132e56fabcefacef10b9cce9c4a663bd3bc0309
dbu <dbu@contraste.com>
2024-05-19 14:34:48 +0100

    refector bookModel

--- models/bookModel.go
+++ models/bookModel.go
@@ -1,37 +1,42 @@
 package models
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
 type Book struct {
 	BookId          uint   `gorm:"primaryKey"`
 	Title           string `gorm:"not null"`
 	Author          string
-	PublicationYear Pubyear `gorm:"type:jsonb"`
+	PublicationYear PubYear `gorm:"type:jsonb"`
 	Isbn            string  `gorm:"size:10;not null;unique"`
 	NumberInShelf   int     //`gorm:"size>0;not null"`
 	NumberBorrowed  int     //`gorm:"size>0;not null"`
 }
 
-type Pubyear struct {
+type PubYear struct {
 	Year  uint
 	Month string
 }
 
-/*// Implement the Scanner interface for Pubyear
-func (py *Pubyear) Scan(value interface{}) error {
+// Implement the Scanner interface for PubYear
+func (py *PubYear) Scan(value interface{}) error {
 	if value == nil {
 		return nil
 	}
 	data, ok := value.([]byte)
 	if !ok {
-		return fmt.Errorf("Failed to scan Pubyear, invalid data type")
+		return fmt.Errorf("Failed to scan PubYear, invalid data type")
 	}
 	return json.Unmarshal(data, py)
 }
 
-// Implement the Valuer interface for Pubyear
-func (py Pubyear) Value() (driver.Value, error) {
+// Implement the Valuer interface for PubYear
+func (py PubYear) Value() (driver.Value, error) {
 	if py.Year == 0 && py.Month == "" {
 		return nil, nil
 	}
 	return json.Marshal(py)
 }
-*/
